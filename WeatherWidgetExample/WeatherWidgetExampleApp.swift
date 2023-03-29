1a1f7b07d67ad6467509f4d61331adb892ba6c2f
David Yoon <qqpo12@naver.com>
2023-03-29 20:18:54 +0900

    Initial Commit

---
+++ WeatherWidgetExample/WeatherWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetExampleApp.swift
+//  WeatherWidgetExample
+//
+//  Created by Jiwon Yoon on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
